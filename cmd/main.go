13d30135729f8b56b6af3fa40ed7c553efc57c91
Tushar Dahiya <tusharxoxoxo@gmail.com>
2024-01-02 15:58:00 +0530

    just pushing away

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+
 type Templates struct{
     templates *template.Template
 }
@@ -22,24 +23,55 @@ func newTemplate() *Templates {
     }
 }
 
+type Contact struct{
+   Name string
+   Email string
+}
+
 type Count struct{
     Count int 
+}
+
+func newContact(name, email string) Contact{
+  return Contact{
+    Name: name,
+    Email: email,
+  }
+}
+
+
+type Contacts = []Contact
+
+type Data struct{
+    Contacts Contacts
+}
+
+func newData() Data{
+    return Data{
+        Contacts: []Contact{
+            newContact("Salman", "chotiheight@driver.com"),
+            newContact("Ashwariya", "robot@shivaji.com"),
+        },
+    }
 }
 
 func main(){
     e := echo.New()
     e.Use(middleware.Logger())
 
-    count := Count{Count : 0}
+    data := newData()
     e.Renderer = newTemplate()
 
     e.GET("/", func (c echo.Context) error{
-        return c.Render(200, "index", count)
+        return c.Render(200, "index", data)
     })
     
-    e.POST("/count", func (c echo.Context) error{
-        count.Count++
-        return c.Render(200, "count", count)
+    e.POST("/contacts", func (c echo.Context) error{
+        name := c.FormValue("name")
+        email := c.FormValue("email")
+
+        data.Contacts = append(data.Contacts, newContact(name, email))
+        return c.Render(200, "display", data)
     })
 
     e.Logger.Fatal(e.Start(":42069"))
